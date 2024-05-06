3309ca7e74a6adda756446ab433a1f9ca7b3a586
Den Smalonski <dzianis.smalonski@protofire.io>
2024-05-06 17:20:23 +0200

    fix: use lint:fix cmd

--- src/utils/arrayReducers.js
+++ src/utils/arrayReducers.js
@@ -1,5 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.reduceToSet = exports.reduceToMap = void 0;
 /**
  * Creates a Map from an array and a key function.
@@ -8,12 +7,12 @@ exports.reduceToSet = exports.reduceToMap = void 0;
  * @returns a map which maps from keys to all list entries, which computed that key for their value
  */
 function reduceToMap(list, keyFunc) {
-    return list.reduce(function (prev, curr) {
-        var _a;
-        var key = keyFunc(curr);
-        prev.has(key) ? (_a = prev.get(key)) === null || _a === void 0 ? void 0 : _a.push(curr) : prev.set(key, [curr]);
-        return prev;
-    }, new Map());
+  return list.reduce(function (prev, curr) {
+    var _a;
+    var key = keyFunc(curr);
+    prev.has(key) ? ((_a = prev.get(key)) === null || _a === void 0 ? void 0 : _a.push(curr)) : prev.set(key, [curr]);
+    return prev;
+  }, new Map());
 }
 exports.reduceToMap = reduceToMap;
 /**
@@ -23,10 +22,10 @@ exports.reduceToMap = reduceToMap;
  * @returns the resulting set
  */
 function reduceToSet(list, valueFunc) {
-    return list.reduce(function (prev, curr) {
-        var value = valueFunc(curr);
-        prev.add(value);
-        return prev;
-    }, new Set());
+  return list.reduce(function (prev, curr) {
+    var value = valueFunc(curr);
+    prev.add(value);
+    return prev;
+  }, new Set());
 }
 exports.reduceToSet = reduceToSet;
