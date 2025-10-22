ccaf88254e57bf21282871e55a4df478802f1f4f
Sebastián Galiano <sgaliano@gmail.com>
2025-10-22 08:31:03 -0300

    feat: add support to Xai Network (Mainnet and Testnet Sepolia) (#9)

--- src/networks.ts
+++ src/networks.ts
@@ -418,4 +418,26 @@ export const networkInfo = new Map<number, NetworkInfo>([
       stagingBaseAPI: 'https://transaction-testnet.staging.safe.kava.io',
     },
   ],
+  [
+    660279,
+    {
+      chainID: 660279,
+      name: 'Xai',
+      shortName: 'xai',
+      currencySymbol: 'XAI',
+      baseAPI: 'https://transaction.safe.xai.games',
+      stagingBaseAPI: 'https://transaction.staging.safe.xai.games',
+    },
+  ],
+  [
+    37714555429,
+    {
+      chainID: 37714555429,
+      name: 'Xai Testnet Sepolia',
+      shortName: 'xaitestnet',
+      currencySymbol: 'sXAI',
+      baseAPI: 'https://transaction-testnet.safe.xai.games',
+      stagingBaseAPI: 'https://transaction-testnet.staging.safe.xai.games',
+    },
+  ],
 ]);
